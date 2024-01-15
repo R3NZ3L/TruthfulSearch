9ffa02cc3699c19e32ef8a0d65fef0db9ccd4383
R3NZ3L <geloaldecoa@gmail.com>
2024-01-15 15:59:35 +0800

    Enabled search across given number of pages
    - Initial search function transferred to a separate function from main (see search_test())
    - Program now asks for n number of pages of search results, returning n x 50 videos
    - TODO: Create and export a dataset (will use pandas/DataFrame)

--- YT_scraper.py
+++ YT_scraper.py
@@ -14,94 +14,109 @@ apiKey = 'AIzaSyCIplXpNgYZ2IS44ZYyEi-hXRu1gzl9I58'
 # Youtube API object
 youtube = build('youtube', 'v3', developerKey=apiKey)
 
-if __name__ == '__main__':
-    searchQuery = input("Search query: ")
 
-    request = youtube.search().list(
+def search_test():
+    searchQuery = input("Search query: ")
+    pages = int(input("Number of pages (50 videos per page): "))
+
+    search_request = youtube.search().list(
         part="snippet",
         q=searchQuery,
         type="video",
         maxResults=50,
-        order="relevance"
+        order="relevance",
+        regionCode="PH"
     )
-
-    response = request.execute()
+    search_response = search_request.execute()
 
     '''
     Response Keys: 
     dict_keys(['kind', 'etag', 'nextPageToken', 'regionCode', 'pageInfo', 'items'])
-    
+
     Search Results (items) Keys: 
     dict_keys(['kind', 'etag', 'id', 'snippet'])
-    
+
     Metadata (snippet) Keys: 
     dict_keys(['publishedAt', 'channelId', 'title', 'description', 'thumbnails', 'channelTitle', 'liveBroadcastContent', 'publishTime']) 
     '''
-
-    searchResults = response.get("items")
+    searchResults = search_response.get("items")
 
     print("--------------")
-    print("")
 
-    for i in range(0, 1):
-        video = searchResults[i]
-        metadata = video.get("snippet")
+    # Returns a number of results; to get multiple results, change range max
+    for n in range(0, pages):
+        print("----------------------- SEARCH PAGE " + str(n + 1) + " -----------------------")
+        for i in range(0, 50):
+            video = searchResults[i]
+            metadata = video.get("snippet")
 
-        print("------------------------------------------------------------------------------")
-        print("[" + str(i + 1) + "]")
-        print("Video ID: " + video.get("id").get("videoId"))
-        print("Title: " + str(metadata.get("title")))
-        print("Channel: " + str(metadata.get("channelTitle") + " (" + metadata.get("channelId") + ")"))
-        # print("Description: " + str(metadata.get("description")))
+            print("[" + str(i + 1) + "]")
+            print("Video ID: " + video.get("id").get("videoId"))
+            print("Title: " + str(metadata.get("title")))
+            print("Channel: " + str(metadata.get("channelTitle") + " (" + metadata.get("channelId") + ")"))
+            # print("Description: " + str(metadata.get("description")))
 
-        request = youtube.videos().list(
-            part=['snippet, statistics'],
-            id=video.get("id").get("videoId"),
-            maxResults=1
-        )
+            request = youtube.videos().list(
+                part=['snippet, statistics'],
+                id=video.get("id").get("videoId"),
+                maxResults=1
+            )
 
-        vid_specs = request.execute()
-        print("----------------------------------------\n", vid_specs)
-        '''
-        Video Specifics Keys:
-        dict_keys(['kind', 'etag', 'items', 'pageInfo'])
-        '''
+            vid_specs = request.execute()
+            print("----------------------------------------")
+            '''
+            Video Specifics Keys:
+            dict_keys(['kind', 'etag', 'items', 'pageInfo'])
+            '''
 
-        # TODO: Display video date of publication
-        print("Video Date of Publication: " + metadata.get("publishedAt")[:10])
-        
-        # TODO: Display Total Views, Likes, and Comments count of Video
-        print("Video View Count: " + vid_specs.get("items")[0].get("statistics").get("viewCount"))
-        print("Video Like Count:" + vid_specs.get("items")[0].get("statistics").get("likeCount"))
-        print("Video Comment Count: " + vid_specs.get("items")[0].get("statistics").get("commentCount"))
+            # Channel information
+            request = youtube.channels().list(
+                part=['snippet', 'statistics'],
+                id=metadata.get("channelId"),
+                maxResults=1
+            )
+            channel_specs = request.execute()
 
-        # TODO: Display video description as raw string
-        print("------------------Start of Description------------------")
-        print("Description:\n" + vid_specs.get("items")[0].get("snippet").get("description"))
-        print("------------------End of Description------------------")
+            print("Channel Subscriber count: " + channel_specs.get("items")[0].get("statistics").get("subscriberCount"))
+            print("Channel Total videos uploaded: " + channel_specs.get("items")[0].get("statistics").get("videoCount"))
+            print(
+                "Channel Date of Publication: " + channel_specs.get("items")[0].get("snippet").get("publishedAt")[:10])
 
-        '''
-        try:
-            print("Transcript: " + str(YouTubeTranscriptApi.get_transcript(video.get("id").get("videoId"))))
-        except:
-            print("Transcript unavailable")
-        finally:
+            print("------------------Start of Description------------------")
+            print("Description:\n" + repr(vid_specs.get("items")[0].get("snippet").get("description")))
+            print("------------------End of Description------------------")
+
+            print("Video Date of Publication: " + metadata.get("publishedAt")[:10])
+            print("Video View Count: " + vid_specs.get("items")[0].get("statistics").get("viewCount"))
+            print("Video Like Count: " + vid_specs.get("items")[0].get("statistics").get("likeCount"))
+
+            try:
+                print("Video Comment Count: " + vid_specs.get("items")[0].get("statistics").get("commentCount"))
+            except:
+                print("Video Comment Count: N/A")
+            '''
+            try:
+                print("Transcript: " + str(YouTubeTranscriptApi.get_transcript(video.get("id").get("videoId"))))
+            except:
+                print("Transcript unavailable")
+            finally:
+                print("")
+            # '''
             print("")
-        # '''
 
-        #TODO get channel information
-        request = youtube.channels().list(
-            part=['snippet','statistics'],
-            id= metadata.get("channelId"),
-            maxResults=1
-        )
+        # Next page, if needed
+        if n != pages:
+            next_page_request = youtube.search().list_next(
+                previous_request=search_request,
+                previous_response=search_response
+            )
+            search_response = next_page_request.execute()
+            searchResults = search_response.get("items")
+            print("")
 
-        channel_specs =request.execute()
 
-        # TODO: Display Subscriber count of channel
-        print("Channel Subscriber count: " + channel_specs.get("items")[0].get("statistics").get("subscriberCount"))
-        # TODO: Display total video uploaded of channel
-        print("Channel Total video uploaded: " + channel_specs.get("items")[0].get("statistics").get("videoCount"))
-        # TODO: Display published date of channel
-        print("Channel Date of Publication: " + channel_specs.get("items")[0].get("snippet").get("publishedAt")[:10])
+if __name__ == '__main__':
+    # Search test
+    search_test()
 
+    # Making the dataset
