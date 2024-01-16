63b5acd2717350e11d61ba89d8efe44161593b46
R3NZ3L <geloaldecoa@gmail.com>
2024-01-16 12:13:15 +0800

    Finalized scraping algorithm
    - IT WORKS
    - Defined "scrape()", where the main scraping algorithm is found
    - Added a loading bar (using tqdm) to make the program more verbose
    
    Changes compared to "search_test()":
    - User can now input a definite number of VIDEOS, instead of pages
    - Additional input is required for filename
    - Scraped video data is exported as a .csv file in the "datasets" folder (a sample dataset is also pushed with this commit)

--- YT_scraper.py
+++ YT_scraper.py
@@ -3,15 +3,20 @@
 # To install, run the following: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-# Youtube Transcript API by Jonas Depoix (2018)
+# YouTube Transcript API by Jonas Depoix (2018)
 # https://github.com/jdepoix/youtube-transcript-api
 # To install, run the following: pip install youtube-transcript-api
 from youtube_transcript_api import YouTubeTranscriptApi
 
+import pandas as pd
+import numpy as np
+from math import ceil
+from tqdm import tqdm
+
 # Put your personal API key here
 apiKey = 'AIzaSyCIplXpNgYZ2IS44ZYyEi-hXRu1gzl9I58'
 
-# Youtube API object
+# YouTube API object
 youtube = build('youtube', 'v3', developerKey=apiKey)
 
 
@@ -91,6 +96,11 @@ def search_test():
             print("Video Like Count: " + vid_specs.get("items")[0].get("statistics").get("likeCount"))
 
             try:
+                print("Video Dislike Count: " + vid_specs.get("items")[0].get("statistics").get("dislikeCount"))
+            except:
+                print("Video Dislike Count: N/A")
+
+            try:
                 print("Video Comment Count: " + vid_specs.get("items")[0].get("statistics").get("commentCount"))
             except:
                 print("Video Comment Count: N/A")
@@ -115,8 +125,145 @@ def search_test():
             print("")
 
 
-if __name__ == '__main__':
-    # Search test
-    search_test()
+def scrape():
+    # Storing data in a numpy array of lists
+    # ------------- SCRAPING ------------- #
+    search_query = input("Search Query: ")
+    num_videos = int(input("Number of Videos: "))
+    filename = input("Filename (.csv): ")
 
-    # Making the dataset
+    print("")
+
+    if num_videos < 50:
+        num_pages = 1
+    else:
+        num_pages = ceil(num_videos / 50)
+
+    search_request = youtube.search().list(
+        part="snippet",
+        q=search_query,
+        type="video",
+        maxResults=50,
+        order="relevance",
+        regionCode="PH"
+    )
+
+    search_response = search_request.execute()
+    search_results = search_response.get("items")
+
+    temp_video_list = []
+    columns = ["video_id", "video_title", "description", "video_dop",
+               "view_count", "like_count", "comment_count",
+               "channel_id", "channel_title", "channel_dop", "sub_count",
+               "total_videos"]
+
+    pbar = tqdm(total=num_videos)
+    pbar.set_description("Scraping...")
+    for n in range(0, num_pages):
+        if num_videos > 50:
+            num_videos -= 50
+            j = 50
+        elif num_videos <= 50:
+            j = num_videos
+
+        for i in range(0, j):
+            # Data from SERP
+            video = search_results[i]
+            metadata = video.get("snippet")
+
+            # Video-specific data
+            request = youtube.videos().list(
+                part=['snippet, statistics'],
+                id=video.get("id").get("videoId"),
+                maxResults=1
+            )
+            vid_specs = request.execute()
+
+            # Channel-specific data
+            request = youtube.channels().list(
+                part=['snippet', 'statistics'],
+                id=metadata.get("channelId"),
+                maxResults=1
+            )
+            channel_specs = request.execute()
+
+            # This list will contain data for one record
+            temp_list = []
+
+            # video_id
+            temp_list.append(video.get("id").get("videoId"))
+
+            # video_title
+            temp_list.append(metadata.get("title"))
+
+            # description
+            temp_list.append(repr(vid_specs.get("items")[0].get("snippet").get("description")))
+
+            # video_dop
+            temp_list.append(metadata.get("publishedAt")[:10])
+
+            # view_count
+            temp_list.append(vid_specs.get("items")[0].get("statistics").get("viewCount"))
+
+            # like_count
+            temp_list.append(vid_specs.get("items")[0].get("statistics").get("likeCount"))
+
+            # comment_count
+            try:
+                temp_list.append(vid_specs.get("items")[0].get("statistics").get("commentCount"))
+            except:
+                temp_list.append("N/A")
+
+            '''
+                        columns = ["video_id", "video_title", "description", "video_dop",
+                           "view_count", "like_count", "comment_count",
+                           "channel_id", "channel_title", "channel_dop", "sub_count",
+                           "total_videos"]
+                        '''
+
+            # channel_id
+            temp_list.append(metadata.get("channelId"))
+
+            # channel_title
+            temp_list.append(metadata.get("channelTitle"))
+
+            # channel_dop
+            temp_list.append(channel_specs.get("items")[0].get("snippet").get("publishedAt")[:10])
+
+            # sub_count
+            temp_list.append(channel_specs.get("items")[0].get("statistics").get("subscriberCount"))
+
+            # total_videos
+            temp_list.append(channel_specs.get("items")[0].get("statistics").get("videoCount"))
+
+            temp_video_list.append(temp_list)
+
+            pbar.update(1)
+
+        # Next page, if needed
+        if n != num_pages:
+            next_page_request = youtube.search().list_next(
+                previous_request=search_request,
+                previous_response=search_response
+            )
+            search_response = next_page_request.execute()
+            search_results = search_response.get("items")
+    pbar.close()
+    temp_nparray = np.array(temp_video_list)
+    # ------------- SCRAPING ------------- #
+
+    # Converting numpy array to DataFrame
+    print("Converting to DataFrame...")
+    df = pd.DataFrame(temp_nparray, columns=columns)
+
+    # Saving to a .csv file
+    df.to_csv("datasets/" + filename + ".csv")
+    print("Complete.")
+
+
+if __name__ == '__main__':
+    # Search test (uncomment line below to test search)
+    # search_test()
+
+    # Making a dataset
+    scrape()
