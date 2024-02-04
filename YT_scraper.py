ac8564bc463d6e1adaf679513db4c2495e33de7c
R3NZ3L <geloaldecoa@gmail.com>
2024-02-04 20:37:26 +0800

    Finalized TOPSIS function for score computation
    - Created and tested the topsis() function in Algo_Test.ipynb, and then transferred to YT_scraper.py
    - Scores and sources are stored in separate .csv files (check covid_philippines folder)
    - Verifiability scores are stored in source_scores.csv

--- YT_scraper.py
+++ YT_scraper.py
@@ -11,6 +11,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import pandas as pd
 import numpy as np
 from math import ceil
+from math import sqrt
 from tqdm import tqdm
 import os
 from time import sleep
@@ -495,14 +496,56 @@ def find_sources(channel_names, channel_IDs):
     sl_df.to_csv("source_links.csv")
 
 
-def get_vs(df):
-    pass
+def topsis(scores, weights):
+    wndm = {}
 
+    for column in weights.keys():
+        temp_list = []
+        x = 0
+        for i in range(0, scores.shape[0]):
+            num = scores.iloc[i][column] ** 2
+            x += num
+        denominator = sqrt(x)
 
-def get_ranking(df):
-    # ------------- SEARCHING ------------- #
-    pass
-    # ------------- SEARCHING ------------- #
+        # Normalize scores
+        for i in range(0, scores.shape[0]):
+            norm_score = scores.iloc[i][column] / denominator
+            temp_list.append(norm_score)
+
+        # Apply weight
+        for i in range(0, len(temp_list)):
+            temp_list[i] *= weights.get(column)
+
+        wndm.update({column: temp_list})
+
+    wndm_df = pd.DataFrame.from_dict(wndm)
+    ideal_best = wndm_df.max()
+    ideal_worst = wndm_df.min()
+
+    dist_from_best = []
+    dist_from_worst = []
+
+    # Euclidean distance from ideal best
+    for i in range(0, wndm_df.shape[0]):
+        temp_num = 0
+        for column in wndm_df.columns:
+            temp_num += (wndm_df.iloc[i][column] - ideal_best[column]) ** 2
+        dist_from_best.append(sqrt(temp_num))
+
+    # Euclidean distance from ideal worst
+    for i in range(0, wndm_df.shape[0]):
+        temp_num = 0
+        for column in wndm_df.columns:
+            temp_num += (wndm_df.iloc[i][column] - ideal_worst[column]) ** 2
+        dist_from_worst.append(sqrt(temp_num))
+
+    vs = []
+    for i in range(0, wndm_df.shape[0]):
+        vs.append(dist_from_worst[i] / (dist_from_best[i] + dist_from_worst[i]))
+
+    vs = pd.Series(np.array(vs))
+
+    return vs
 
 
 
@@ -558,6 +601,19 @@ if __name__ == '__main__':
                 os.chdir(os.getcwd() + "/datasets/" + filename)
 
                 print("Working @ " + os.getcwd())
+
+                ss_df = pd.read_csv("source_scores.csv")
+                ss_df.drop("Unnamed: 0", axis=1, inplace=True)
+                print(f"File {filename} found.")
+
+                weights = {
+                    "profiles": 0.50,
+                    "website": 0.35,
+                    "social_media_presence": 0.15
+                }
+
+                ss_df["vs"] = topsis(ss_df, weights)
+                ss_df.to_csv("source_scores.csv")
 
                 # Return to main folder
                 os.chdir("..")
