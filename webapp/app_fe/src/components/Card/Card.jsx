fca53267fc98462e728d57794760c00567ce0a16
Marc Gabriel Baura <marcbaura@gmail.com>
2024-02-11 22:12:20 +0800

    Added different possible colors and score ranges for front-end

--- webapp/app_fe/src/components/Card/Card.jsx
+++ webapp/app_fe/src/components/Card/Card.jsx
@@ -1,13 +1,27 @@
-import random_thumbnail from '../../assets/random_thumbnail.jpg'
+import random_thumbnail from '../../assets/random_thumbnail.jpg';
 import {useState} from 'react';
-import './Card.css'
+import './Card.css';
+import PropTypes from 'prop-types';
 
-function Card() {
+function Card(props) {
 
-    const [visibleModal, setVisibleModal] = useState(false)
+    const [visibleModal, setVisibleModal] = useState(false);
 
-    const showModal = () => setVisibleModal(true);
-    const hideModal = () => setVisibleModal(false);
+    function convertScore() {
+        return  props.score == 1 ? "0-19%"  : 
+                props.score == 2 ? "20-39%" : 
+                props.score == 3 ? "40-59%" :
+                props.score == 4 ? "60-79%" :
+                props.score == 5 ? "80-100%" : "0-19%"
+    }
+
+    const backgroundColor = {
+        "backgroundColor":  props.score == 1 ? "red"        : 
+                            props.score == 2 ? "orange"     : 
+                            props.score == 3 ? "yellow"     :
+                            props.score == 4 ? "lightblue"  :
+                            props.score == 5 ? "lightgreen" : "black"
+    } 
 
     return (
         <>
@@ -23,8 +37,8 @@ function Card() {
                         </div>
                     </div>
                     Channel name 
-                    <span className="score" onMouseOver={() => setVisibleModal(true)} onMouseLeave={() => setVisibleModal(false)}>
-                        59%
+                    <span className="score" style={backgroundColor} onMouseOver={() => setVisibleModal(true)} onMouseLeave={() => setVisibleModal(false)}>
+                        {convertScore(props.score)}
                         { visibleModal && 
                             <span className="channel_info_modal">
                                 <p>The channel got a verifiability score of 59% due to these reasons:</p>
@@ -41,6 +55,14 @@ function Card() {
         </div>
         </>
     )
+}
+
+Card.propTypes = {
+    score: PropTypes.number,
+}
+
+Card.defaultProps = {
+    score: "1",
 }
 
 export default Card;
--- webapp/app_fe/src/components/HomePage/HomePage.jsx
+++ webapp/app_fe/src/components/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import './HomePage.css'
 function HomePage() {
 
     const [topic, setTopic] = useState("covid") // default covid
-    const possibleTopics = ["covid", "flat-earth", "aliens"] 
+    const possibleTopics = ["covid", "flat-earth", "alien-life"] 
     
     const changeTopic = (e) => {
         if (possibleTopics.includes(e.target.value)) {
@@ -15,20 +15,28 @@ function HomePage() {
         }
     }
 
+    /* Possible scores for now
+        1 = 0-19%
+        2 = 20-39%
+        3 = 40-59%        
+        4 = 60-79%
+        5 = 80-100%
+    */
+
     return (
         <>
         <select id="topic_choices" onChange={(e) => changeTopic(e)}>
             <option className="option" value="covid">Covid-19</option>
             <option className="option" value="flat-earth">Flat Earth</option>
-            <option className="option" value="aliens">Covid</option>
+            <option className="option" value="alien-life">Alien Life</option>
         </select>
 
         <p>Current chosen topic is: {topic}</p>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card score={1} />
+        <Card score={2} />
+        <Card score={3} />
+        <Card score={4} />
+        <Card score={5} />
         </>
     );
 
