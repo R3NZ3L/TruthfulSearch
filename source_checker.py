10cc3d5e4541310202a60c368aaa834bd3396c45
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2024-03-13 20:25:04 +0800

    Update source_checker.py
    Added apiKey and cseKey

--- source_checker.py
+++ source_checker.py
@@ -19,11 +19,13 @@ import os
 # apiKey = 'AIzaSyCIplXpNgYZ2IS44ZYyEi-hXRu1gzl9I58' # Aldecoa
 # apiKey = 'AIzaSyCJBMIMpGpBdmTkx7SRhObSNAyV_aRSjho' # Aquino
 apiKey = 'AIzaSyBTRvkhM6ESdLHu0djfP39-IKHufQogxOI' # Baura
+# apiKey = 'AIzaSyA7eqxwuzM6SUDDVTss6DSzKGEt7kSJesg' # Sevillana
 
 # Search engine ID
 # cseKey = "23c1c70a203ac4852" # Aldecoa
 # cseKey = "a7c987e23f0fe448e" # Aquino
 cseKey = "76c19208b12de4763" # Baura
+# cseKey = "a674809398a7b46df" # Sevillana
 
 # Google Custom Search API
 google_resource = build("customsearch", "v1", developerKey=apiKey).cse()
