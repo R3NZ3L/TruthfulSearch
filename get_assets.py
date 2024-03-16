b0418c5d62519ae4576dfec1df301b454a02f09c
Marc Gabriel Baura <121290941+MarcGabrielBaura@users.noreply.github.com>
2024-03-16 22:58:19 +0800

    Updated datasets with thumbnails and channel profile links

--- get_assets.py
+++ get_assets.py
@@ -17,16 +17,21 @@ youtube = build('youtube', 'v3', developerKey=apiKey)
 
 
 def yt_assets_scrape():
-    
-    videos_path = "datasets/" + "covid_philippines" + "/videos.csv"
-    channels_path = "datasets/" + "covid_philippines" + "/channels.csv"
+
+    filename = input("Filename (w/o .csv): ")
+    path = os.getcwd() + "/datasets/" + filename
+    os.chdir(path)
+
+    videos_path =  "videos.csv"
+    channels_path = "channels.csv"
 
     videos_df = pd.read_csv(videos_path, usecols=['video_id'])['video_id'].tolist()
+    complete_videos_df = pd.read_csv(videos_path).drop('Unnamed: 0', axis=1)
     channels_df = pd.read_csv(channels_path, usecols=['channel_id'])['channel_id'].tolist()
-    videos_df = pd.read_csv(videos_path)
-    channels_df = pd.read_csv(channels_path)
+    complete_channels_df = pd.read_csv(channels_path).drop('Unnamed: 0', axis=1)
+
     thumbnail_links = []
-    """
+    
     while (len(videos_df) > 0):
         if(len(videos_df) >= 50):
 
@@ -49,11 +54,10 @@ def yt_assets_scrape():
                 except:
                     thumbnail_links.append('https://play-lh.googleusercontent.com/zaMefYVID82FrctnM3g2b9Ul1Wk9cAR1aYfKNq_uvHnDbGo2wqZgliYVioi8Fa3YTA')
 
-    pd.Series(thumbnail_links).to_csv('thumbnail_links', index=False, header=False)
-    """
+    complete_videos_df['thumbnail'] = pd.Series(thumbnail_links)
 
-    """
     channel_profile_links = []
+
     while (len(channels_df) > 0):
         if(len(channels_df) >= 50):
             channels_request = youtube.channels().list(
@@ -69,36 +73,22 @@ def yt_assets_scrape():
             channels_df = channels_df[len(channels_df):]
 
         channels_data = channels_request.execute()
+
         for channel in channels_data.get('items'):
                 try:
                     channel_profile_links.append(channel.get('snippet').get('thumbnails').get('medium').get('url'))
                 except:
                     channel_profile_links.append('https://play-lh.googleusercontent.com/zaMefYVID82FrctnM3g2b9Ul1Wk9cAR1aYfKNq_uvHnDbGo2wqZgliYVioi8Fa3YTA')
 
-    pd.Series(channel_profile_links).to_csv('channel_profile_links', index=False, header=False)
-    """
+   
+    complete_channels_df['profile'] = pd.Series(channel_profile_links)
 
-    """
-    videos_path = "datasets/" + "covid_philippines" + "/videos.csv"
-    channels_path = "datasets/" + "covid_philippines" + "/channels.csv"
-    videos_df = pd.read_csv(videos_path)
-    channels_df = pd.read_csv(channels_path)
-    videos_df['thumbnail'] = pd.read_csv("thumbnail_links.csv", header=None)
-    channels_df['profile'] = pd.read_csv("channel_profile_links.csv", header=None)
-    print(channels_df)
     
-    path = os.getcwd() + "/datasets/" + 'covid_philippines'
-    os.chdir(path)
-
-    videos_df.to_csv('videos.csv', index=False)
-    channels_df.to_csv('channels.csv', index=False)
-
+    complete_videos_df.to_csv('videos.csv')
+    complete_channels_df.to_csv('channels.csv')
+   
     os.chdir("..")
     os.chdir("..")
     print("Back @ " + os.getcwd())
-    """
 
-    channels_path = "datasets/" + "covid_philippines" + "/channels.csv"
-    channels_df = pd.read_csv(channels_path)
-    print(videos_df.tail(10))
 yt_assets_scrape()
