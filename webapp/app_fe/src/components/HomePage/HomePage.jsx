4e99c3eded589c83dbbb061960f3448a43366e63
Marc Gabriel Baura <121290941+MarcGabrielBaura@users.noreply.github.com>
2024-06-03 21:22:23 +0800

    updated design

--- webapp/app_fe/src/components/HomePage/HomePage.jsx
+++ webapp/app_fe/src/components/HomePage/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage() {
     function getData() {
         fetch("http://127.0.0.1:105/api/data_main?topic=" + topic + '&sort=' + sortResultBy, {method: 'GET'}) // can  sort by 'verifiability' or 'upload_date'
         .then(response => response.json())
-        .then(data => setVideoData(data))
+        .then(data => setVideoData(data)).then(setVideoShown(0))
         .catch((error) => console.log('data fetching failed!, Here is the error: \n' + error));
         console.log('Get Data happened')
     }
@@ -43,29 +43,36 @@ function HomePage() {
 
     return (
         <>
-        <span id="brand">VERIFYVID</span>
-        <div id="search_bar">
+        <div id="header">
+            <div id="brand">VERIFYVID</div>
+            <div id="search_bar">
             <select id="topic_choices" onChange={(e) => changeTopic(e)}>
                 <option className="option" value="covid_philippines">Covid Philippines</option>
                 <option className="option" value="covid_vaccine">Covid Vaccine</option>
                 <option className="option" value="israel_palestine_conflict_history">Israel Palestine Conflict History</option>
             </select>
             <button onClick={() => getData()}>&#128269;</button>
+            </div>
+            <div></div>
         </div>
         <Dropdown sortResultBy={sortResultBy} setSortResultBy={setSortResultBy} />
         <main> 
-            <div id="video-container">
-                {videoData && videoData.map((video, index) => <Video onClick={() => {
-                        setVideoShown(index);
-                    }} 
-                    toggled={index == videoShown} key={video.video_id+index} score={2} videoInfo={video}/>)
-                }
+        <div id="video-container">
+            {videoData && videoData.map((video, index) => <Video onClick={() => {
+                    setVideoShown(index);
+                }} 
+                toggled={index == videoShown} key={video.video_id+index} score={2} videoInfo={video}/>)
+            }
             </div>
-            {videoData && <InfoCard video={videoData[videoShown]} videoPosition={document.getElementsByClassName('video')[videoShown].offsetTop - document.getElementById('video-container').offsetTop}/>}
+            {videoData && <InfoCard video={videoData[videoShown]} videoPosition={
+                document.getElementsByClassName('video')[videoShown] && document.getElementsByClassName('video')[videoShown].offsetTop - document.getElementById('video-container').offsetTop
+            }
+            />}
         </main>
         </>
     );
 
+    
 }
 
 export default HomePage
