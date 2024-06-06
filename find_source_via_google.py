ceded2875b9ceedbf09e4925cca997e7092fecf0
R3NZ3L <geloaldecoa@gmail.com>
2024-06-06 17:59:41 +0800

    Progress in find_source_via_google.py, minor cleaning of scraped titles from Google SERP

--- find_source_via_google.py
+++ find_source_via_google.py
@@ -9,9 +9,6 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 import regex as re
-import requests
-from bs4 import BeautifulSoup
-import json
 import os
 
 # Put your personal API key here
@@ -32,4 +29,268 @@ google_resource = build("customsearch", "v1", developerKey=apiKey).cse()
 
 quota_reached = False
 stopped_at = None
+
+
+def find_linkedIn(channel_name, query):
+    found = False
+    pattern = r'https:\/\/(www\.)?linkedin\.com\/(company|in)\/.+'  # Used to find specific profile links
+
+    try:
+        li_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            link = li_response.get("items")[i].get("formattedUrl")
+            if re.search(pattern, link) is not None:
+                # Get profile name from search result
+                match = re.search(r'\w+\s(\w+)?', li_response.get("items")[i].get("title"))
+                if match is not None:
+                    profile_name = match.group()
+
+                    # Get similarity between found profile name and channel name
+                    # This is to prevent false positives in finding a LinkedIn profile
+                    similarity = round(jaro_similarity(channel_name.lower(), profile_name.lower()), 2)
+
+                    # If n% similar, consider LinkedIn profile as found
+                    if similarity >= 0.80:
+                        found = True
+                        break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_wiki(channel_name, query):
+    found = False
+    pattern = r'https:\/\/\w{2}.wikipedia\.org\/wiki\/.+'
+
+    try:
+        wiki_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            link = wiki_response.get("items")[i].get("formattedUrl")
+            if re.search(pattern, link) is not None:
+                # Get Wiki page name from search result
+                title = wiki_response.get("items")[i].get("title")
+                match = re.search(r'.+(?=\s-\sWikipedia)', title)
+                if match is not None:
+                    page_name = match.group()
+
+                    # Get similarity between found Wiki page name and channel name
+                    # This is to prevent false positives in finding a Wiki page
+                    similarity = round(jaro_similarity(channel_name.lower(), page_name.lower()), 2)
+
+                    # If n% similar, consider Wiki page as found
+                    if similarity >= 0.80:
+                        found = True
+                        break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_website(channel_name, query):
+    found = False
+    # RegEx to exclude YouTube, LinkedIn, and Wikipedia pages
+    pattern = r'https\:\/\/(\w{2}.wikipedia\.org\/wiki\/.+|www\.(youtube\.com.+|linkedin\.com.+))'
+
+    try:
+        website_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            title = website_response.get("items")[i].get("title")
+            link = website_response.get("items")[i].get("link")
+            if channel_name.lower() in title.lower():
+                if re.search(pattern, link) is None:
+                    # The first result among the filtered at this point is MOST LIKELY the official website
+                    found = True
+                    break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_fb(channel_name, query):
+    found = False
+    pattern = r'^https\:\/\/(www\.)?facebook\.com\/.+\/'
+
+    try:
+        fb_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            link = fb_response.get("items")[i].get("formattedUrl")
+            if re.search(pattern, link) is not None:
+                title = fb_response.get("items")[i].get("title")
+                similarity = round(jaro_similarity(channel_name.lower(), title.lower()), 2)
+
+                if similarity >= 0.80:
+                    found = True
+                    break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_twitter(channel_name, query):
+    found = False
+    pattern = r'https\:\/\/(twitter|x)\.com\/.+'
+
+    try:
+        twitter_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            link = twitter_response.get("items")[i].get("formattedUrl")
+            if re.search(pattern, link) is not None:
+                title = twitter_response.get("items")[i].get("title")
+                # Remove filler text after actual name of profile
+                title = re.sub(r'\s\(@.+', "", title)
+                similarity = round(jaro_similarity(channel_name.lower(), title.lower()), 2)
+
+                if similarity >= 0.80:
+                    found = True
+                    break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_instagram(channel_name, query):
+    found = False
+    pattern = r'https:\/\/(www\.)?instagram\.com\/.+'
+
+    try:
+        instagram_response = google_resource.list(
+            q=query,
+            cx=cseKey
+        ).execute()
+
+        for i in range(0, 10):
+            link = instagram_response.get("items")[i].get("formattedUrl")
+            if re.search(pattern, link) is not None:
+                title = instagram_response.get("items")[i].get("title")
+                # Remove filler text after actual name of profile
+                title = re.sub(r'\s\(@.+', "", title)
+                similarity = round(jaro_similarity(channel_name.lower(), title.lower()), 2)
+
+                if similarity >= 0.80:
+                    found = True
+                    break
+    except HttpError:
+        global quota_reached
+        global stopped_at
+        if not quota_reached:
+            quota_reached = True
+            stopped_at = (channel_name, query)
+
+    if not found:
+        return found, np.nan
+    else:
+        return found, link
+
+
+def find_sources_via_google(sc, sl):
+    global quota_reached
+
+    # Building queries for missing sources
+    queries = []
+
+    for i in range(sc.shape[0]):
+        for column in sc.columns[2:]:
+            if not sc.iloc[i][column]:
+                channel_name = sc.iloc[i]["channel_name"]
+                # print(f"{channel_name} {column}")
+                queries.append((i, channel_name, column))
+
+    print(f"Trying to look for {len(queries)} missing sources via Google...")
+
+    for i in range(len(queries)):
+        '''
+        TODO:
+        - Pop query from queries
+        - Use appropriate function to check for the right source
+        - If found, update source_check and source_links accordingly
+        - If quota_reached, save queries as a file (maybe .csv)
+        '''
+        query = queries.pop(0)
+        # print(f"{query[1]} {query[2]}")
+
+        if not quota_reached:
+            pass
+        else:
+            # Save queries as .csv
+            pass
+
+    print(f"Queries left: {len(queries)}")
+
+
+
+if __name__ == '__main__':
+    filename = input("Filename (w/o .csv): ")
+    path = os.getcwd() + "/datasets/" + filename
+    os.chdir(path)
+
+    print("Reading source checklist...")
+    sc = pd.read_csv("source_check.csv", index_col=0)
+
+    print("Reading source links...")
+    sl = pd.read_csv("source_links.csv", index_col=0)
+
+    find_sources_via_google(sc, sl)
+
+    os.chdir("..")
+
 
