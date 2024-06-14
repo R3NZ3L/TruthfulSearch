25929d77dc2eff6ed14128a051b3349f23106565
JohnMarcSevillana <john_marc_sevillana@dlsu.edu.ph>
2024-06-14 16:05:03 +0800

    Update find_source_via_google.py

--- find_source_via_google.py
+++ find_source_via_google.py
@@ -19,13 +19,13 @@ import time
 #apiKey='AIzaSyDZlAPC29rxMjh-gu-ZfgTPb8wrRLglrs4' #2ndacct Aquino
 #apiKey ='AIzaSyAZxwwAfgVoTSys4HCiirvs-h0ynph-rmU' #3rd aquino
 # apiKey = 'AIzaSyDKBteFADAddCvCCvEKrfJRFSxc-ovflsQ' # Baura
-# apiKey = 'AIzaSyDH6SzpJVTOzf_RC2j6Pq08KG6zPa3itwE' # Sevillana
+apiKey = 'AIzaSyAPvryNBQ3TkXwqBJdW9_0AkhhCYZRtA2c' # Sevillana
 
 # Search engine ID
 # cseKey = "23c1c70a203ac4852" # Aldecoa
-cseKey = "a7c987e23f0fe448e" # Aquino
+# cseKey = "a7c987e23f0fe448e" # Aquino
 # cseKey = "05e988b88095c4deb" # Baura
-#cseKey = "5269d103bb52e4488" # Sevillana
+cseKey = "f465f751a15a34c35" # Sevillana
 
 # Google Custom Search API
 google_resource = build("customsearch", "v1", developerKey=apiKey).cse()
